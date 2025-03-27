658bc82e323b39f6788a6fa379e8e23d34e4322b
Maicon Barbosa <141081769+htmlmaicon@users.noreply.github.com>
2025-03-27 15:10:57 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 require_once 'bd.php';
 
 class Produto {
@@ -8,14 +8,15 @@ class Produto {
         $this->pdo = $pdo_produtos;
     }
 
-    public function adicionar($nome, $descricao, $preco, $quantidade) {
+    public function adicionar($nome, $descricao, $preco, $quantidade, $imagem_url) {
         try {
-            $stmt = $this->pdo->prepare("INSERT INTO produtos (nome, descricao, preco, quantidade) VALUES (:nome, :descricao, :preco, :quantidade)");
+            $stmt = $this->pdo->prepare("INSERT INTO produtos (nome, descricao, preco, quantidade, imagem_url) VALUES (:nome, :descricao, :preco, :quantidade, :imagem_url)");
             $stmt->execute([
                 ':nome' => $nome,
                 ':descricao' => $descricao,
                 ':preco' => $preco,
-                ':quantidade' => $quantidade
+                ':quantidade' => $quantidade,
+                ':imagem_url' => $imagem_url
             ]);
             return "Produto cadastrado com sucesso!";
         } catch (PDOException $e) {
@@ -33,14 +34,15 @@ class Produto {
         return $stmt->execute([':id' => $id]);
     }
 
-    public function editar($id, $nome, $descricao, $preco, $quantidade) {
-        $stmt = $this->pdo->prepare("UPDATE produtos SET nome=:nome, descricao=:descricao, preco=:preco, quantidade=:quantidade WHERE id=:id");
+    public function editar($id, $nome, $descricao, $preco, $quantidade, $imagem_url) {
+        $stmt = $this->pdo->prepare("UPDATE produtos SET nome=:nome, descricao=:descricao, preco=:preco, quantidade=:quantidade, imagem_url=:imagem_url WHERE id=:id");
         return $stmt->execute([
             ':id' => $id,
             ':nome' => $nome,
             ':descricao' => $descricao,
             ':preco' => $preco,
-            ':quantidade' => $quantidade
+            ':quantidade' => $quantidade,
+            ':imagem_url' => $imagem_url
         ]);
     }
 }
